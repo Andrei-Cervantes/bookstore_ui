a30919cd875a1e4d5ee15c4d78db1a7a78ad88c8
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-07-24 11:01:24 +0800

    refactor routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import CreateBook from './pages/CreateBooks'
-import ShowBook from './pages/ShowBook'
-import EditBook from './pages/EditBook'
-import DeleteBook from './pages/DeleteBook'
+import AppRoutes from "./Routes/appRoutes";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-    </Routes>
-  )
-}
+  return <AppRoutes />;
+};
 
-export default App
+export default App;
