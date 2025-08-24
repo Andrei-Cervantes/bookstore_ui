9de9736cde4faa211595eea4ad10d9381493d532
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-08-24 17:29:57 +0800

    add login form functionality to handle login endpoint

--- src/features/public/login/components/LoginForm.tsx
+++ src/features/public/login/components/LoginForm.tsx
@@ -1,5 +1,58 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import useAuthService from "@/shared/services/authService";
+import { useNavigate } from "react-router-dom";
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
+
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  // Login Service Mutation
+  const { login: loginService } = useAuthService();
+  const loginMutation = useMutation({
+    mutationFn: (data: LoginSchema) => loginService(data),
+    onSuccess: () => {
+      navigate("/");
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const onSubmit = (data: LoginSchema) => loginMutation.mutate(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input type="email" placeholder="Email" {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default LoginForm;
--- src/features/public/login/pages/Login.tsx
+++ src/features/public/login/pages/Login.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "../components/LoginForm";
+
 const Login = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center bg-background">
       <h1 className="text-2xl font-bold text-primary">Login</h1>
+      <LoginForm />
     </div>
   );
 };
