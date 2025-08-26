cbac5d69f7fdc918e9f7ee26b02e82d98cd91cbc
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-08-26 14:02:53 +0800

    fix styling for register page

--- src/features/public/register/components/RegisterForm.tsx
+++ src/features/public/register/components/RegisterForm.tsx
@@ -4,6 +4,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import useAuthService from "@/shared/services/authService";
 import { useNavigate } from "react-router-dom";
+import InputPassword from "@/shared/components/common/InputPassword";
+import { Input } from "@/shared/components/ui/input";
+import { Button } from "@/shared/components/ui/button";
 
 const registerSchema = z
   .object({
@@ -46,32 +49,37 @@ const RegisterForm = () => {
   const onSubmit = (data: RegisterSchema) => registerMutation.mutate(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex flex-col gap-2"
+    >
       <div>
-        <input type="text" placeholder="Name" {...register("name")} />
+        <Input type="text" placeholder="Name" {...register("name")} />
         {errors.name && <p>{errors.name.message}</p>}
       </div>
       <div>
-        <input type="email" placeholder="Email" {...register("email")} />
+        <Input type="email" placeholder="Email" {...register("email")} />
         {errors.email && <p>{errors.email.message}</p>}
       </div>
       <div>
-        <input
-          type="password"
+        <InputPassword
           placeholder="Password"
           {...register("password")}
+          withIcon={false}
         />
         {errors.password && <p>{errors.password.message}</p>}
       </div>
       <div>
-        <input
-          type="password"
+        <InputPassword
           placeholder="Confirm Password"
           {...register("confirmPassword")}
+          withIcon={false}
         />
         {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
       </div>
-      <button type="submit">Register</button>
+      <Button type="submit" className="mt-4">
+        Register
+      </Button>
     </form>
   );
 };
--- src/shared/components/common/InputPassword.tsx
+++ src/shared/components/common/InputPassword.tsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import { Input } from "../ui/input";
 import { Eye, EyeOff, Lock } from "lucide-react";
+import { cn } from "@/shared/lib/utils";
 
-const InputPassword = ({ ...props }: React.ComponentProps<"input">) => {
+interface Props extends React.ComponentProps<"input"> {
+  withIcon?: boolean;
+}
+
+const InputPassword = ({ withIcon = true, ...props }: Props) => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="relative">
-      <Lock className="text-primary absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4" />
+      {withIcon && (
+        <Lock className="text-primary absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4" />
+      )}
       <Input
         type={showPassword ? "text" : "password"}
         {...props}
-        className="pl-8"
+        className={cn(withIcon && "pl-8")}
       />
       <button
         type="button"
         onClick={() => setShowPassword(!showPassword)}
-        className="absolute right-2 top-1/2 -translate-y-1/2 transition-all duration-300"
+        className="absolute right-2 top-1/2 -translate-y-1/2 transition-all duration-300 hover:cursor-pointer hover:scale-105"
       >
         {showPassword ? (
           <EyeOff className="text-primary" />
