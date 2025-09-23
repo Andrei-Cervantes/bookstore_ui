dd5c36dde437e6261d1316dd302d722494075263
andrei <andrei29.cervantes@gmail.com>
2025-09-23 23:45:52 +0800

    fix tab index for password inputs

--- src/shared/components/common/InputPassword.tsx
+++ src/shared/components/common/InputPassword.tsx
@@ -21,6 +21,7 @@ const InputPassword = ({ withIcon = true, ...props }: Props) => {
         className={cn(withIcon && "pl-8")}
       />
       <button
+        tabIndex={-1}
         type="button"
         onClick={() => setShowPassword(!showPassword)}
         className="absolute right-2 top-1/2 -translate-y-1/2 transition-all duration-300 hover:cursor-pointer hover:scale-105"
